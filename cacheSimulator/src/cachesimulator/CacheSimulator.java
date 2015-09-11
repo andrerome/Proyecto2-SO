78d3c2345235b54f4f1116e1d5232653d86ca1fa
anjoroja <anjoroja@gmail.com>
2015-09-11 16:06:11 -0500

    Función para leer el archivo workload.txt

--- cacheSimulator/src/cachesimulator/CacheSimulator.java
+++ cacheSimulator/src/cachesimulator/CacheSimulator.java
@@ -5,6 +5,13 @@
  */
 package cachesimulator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Andres
@@ -24,12 +31,40 @@ public class CacheSimulator {
             System.out.println("Resultados:");
             System.out.println("\tMiss rate:              x% (W misses out of Q references)");
             System.out.println("\tMiss rate (warm cache): y% (M misses out of Q-(cache size) references)");
+            readFile(args[0]);
         }else{
             System.out.println("Ejecutar por línea de comando con los siguientes argumentos:");
             System.out.println("<workload_file> <policy> <cache size>");
             System.out.println("Ejemplo: $java cacheSimulator workload.txt LRU 50000");
         }
    
+    }
+    
+    public static void readFile(String filename){
+        File file=null;
+        FileReader reader=null;
+        BufferedReader buffer=null;
+        //Se podría usar clase Scanner.java para probar eficiencia en la lectura.
+        
+        try{
+           file=new File(filename);
+           reader=new FileReader(file);
+           buffer=new BufferedReader(reader);
+           
+           String line;
+           while((line=buffer.readLine())!=null)
+              System.out.println(line);
+        }
+        catch(Exception e){
+           System.out.println("Error al leer el archivo: "+e.getMessage());
+        }finally{
+            try {
+                if(reader!=null)   
+                   reader.close();
+            } catch (Exception ex) {
+                System.out.println("Error al cerrar el archivo: "+ex.getMessage());
+            }
+        }
     }
    
     
