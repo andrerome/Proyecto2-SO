8396d81e104bdc635395a221278cf8461f5aa9ae
anjoroja <anjoroja@gmail.com>
2015-09-12 14:04:37 -0500

    Aplicación del algoritmo LRU usando LinkedHashMap.

--- cacheSimulator/src/algorithms/LRU.java
+++ cacheSimulator/src/algorithms/LRU.java
@@ -5,13 +5,46 @@
  */
 package algorithms;
 
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 /**
  *
  * @author Andres
  */
-public class LRU {
+public class LRU extends LinkedHashMap{
 
-    public LRU() {
+  private int entrys;
+  private int lines=0;
+  private int hits=0;
+
+  public LRU(int entrys){
+    super(entrys, 0.75f, true);
+    this.entrys = entrys;
+  }
+
+  public Object get(Object key){
+    lines++;
+    if(containsKey(key)){
+      hits++;
     }
-    
+    Object value=super.get(key);
+    return value;
+  }
+
+  protected boolean removeEldestEntry(Entry eldest){
+    return (size()>entrys);
+  }
+
+    public int getEntrys() {
+        return entrys;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public int getLines() {
+        return lines;
+    }
+   
 }
--- cacheSimulator/src/cachesimulator/CacheSimulator.java
+++ cacheSimulator/src/cachesimulator/CacheSimulator.java
@@ -5,10 +5,12 @@
  */
 package cachesimulator;
 
+import algorithms.LRU;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,14 +26,27 @@ public class CacheSimulator {
     public static void main(String[] args) {
         // TODO code application logic here
         if(args.length==3){
+            
             System.out.println("File: "+args[0]);
             System.out.println("Policy: "+args[1]);
             System.out.println("Cache size: "+args[2]);
-            System.out.println("Evaluando una caché "+args[1]+" con "+args[2]+" entradas.");
-            System.out.println("Resultados:");
-            System.out.println("\tMiss rate:              x% (W misses out of Q references)");
-            System.out.println("\tMiss rate (warm cache): y% (M misses out of Q-(cache size) references)");
-            readFile(args[0]);
+            System.out.println("Evaluando una caché "+args[1]+" con "+args[2]+" entradas...");
+            
+            switch (args[1]) {
+                case "LRU":
+                    LRUcache(args[0],Integer.parseInt(args[2]));
+                    break;
+                case "OPTIMO":
+                    break;
+                case "CLOCK":
+                    break;
+                case "VARIANTE":
+                    break;
+                default:
+                    System.out.println(args[1]+" no es una política de desalojo de caché válida!");
+                    break;
+            }
+
         }else{
             System.out.println("Ejecutar por línea de comando con los siguientes argumentos:");
             System.out.println("<workload_file> <policy> <cache size>");
@@ -40,20 +55,31 @@ public class CacheSimulator {
    
     }
     
-    public static void readFile(String filename){
+    public static void LRUcache(String filename, int entrys){
         File file=null;
         FileReader reader=null;
         BufferedReader buffer=null;
         //Se podría usar clase Scanner.java para probar eficiencia en la lectura.
-        
+        LRU lru=new LRU(entrys);
+        DecimalFormat df = new DecimalFormat("0.00");
+        float hitrate=0;
         try{
            file=new File(filename);
            reader=new FileReader(file);
            buffer=new BufferedReader(reader);
            
            String line;
-           while((line=buffer.readLine())!=null)
-              System.out.println(line);
+           while((line=buffer.readLine())!=null){
+            String filedata = (String)lru.get(line);
+                if (filedata == null){
+                    lru.put(line,filedata);
+                }  
+            }
+            hitrate=(float)lru.getHits()*100/lru.getLines();
+            System.out.println("Resultados:");
+            System.out.println("\tMiss rate:              "+df.format(100-hitrate)+"% ( "+(lru.getLines()-lru.getHits())+" misses out of Q references)");
+            System.out.println("\tHit rate (warm cache):  "+df.format(hitrate)+"% ( "+lru.getHits()+" hits out of Q-(cache size) references)");
+              
         }
         catch(Exception e){
            System.out.println("Error al leer el archivo: "+e.getMessage());
